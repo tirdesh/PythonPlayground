3d5e96d5ec04fcd2cfd2868d8776ecf5caf0420d
tirdesh <71664201+tirdesh@users.noreply.github.com>
2024-03-23 21:11:29 -0400

    Update Contains Duplicate.py

--- NeetCode Blind75/Contains Duplicate.py
+++ NeetCode Blind75/Contains Duplicate.py
@@ -1,5 +1,7 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        # Time complexity: O(n) - Looping through the list once
+        # Space complexity: O(n) - Using a set to store unique elements
         s= set()
         for i in sorted(nums):
             if i in s:
