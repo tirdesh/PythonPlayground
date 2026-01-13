9ddd9ba2097e335ddd2c4f332f8cd437ea76ab5d
tirdesh <tirdesh@gmail.com>
2026-01-13 17:45:44 -0600

    Time: 0 ms (100.00%) | Memory: 19.5 MB (6.51%) - LeetSync

---
+++ 551-student-attendance-record-i/student-attendance-record-i.py
@@ -0,0 +1,21 @@
+class Solution:
+    def checkRecord(self, s: str) -> bool:
+        return s.count('A')<2 and 'LLL' not in s
+        acount = 0
+        i=0
+        while i<len(s):
+            if s[i] == "A":
+                acount+=1
+                if acount>=2:
+                    return False
+                i+=1
+            elif s[i] == "L":
+                lcount=0
+                while i<len(s) and s[i] == "L":
+                    lcount+=1
+                    i+=1
+                    if lcount==3:
+                        return False
+            else:
+                i+=1
+        return True
