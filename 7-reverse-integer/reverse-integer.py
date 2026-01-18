617be2a25422be0853720c48b60ab5db54d90fc1
tirdesh <tirdesh@gmail.com>
2026-01-18 14:19:48 -0600

    Time: 53 ms (5.09%) | Memory: 19.3 MB (7.88%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -1,5 +1,6 @@
 class Solution:
     def reverse(self, x: int) -> int:
+        """
         sign = -1 if x<0 else 1
         x_List = list(str(abs(x)))
         l = 0
@@ -13,4 +14,15 @@ class Solution:
             return 0
         else:
             return sign*ans
+        """
+        limit = 2**31
+        sign = -1 if x<0 else 1
+        x = abs(x)
+        res = 0
+        while x!=0:
+            pop = x%10
+            res = (res *10)+ pop
+            x=x//10
+        res *= sign
+        return res if -limit<=res<=limit else 0
             
