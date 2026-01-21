a74b0ce92817d4d4654a61033373da2684812e0d
tirdesh <tirdesh@gmail.com>
2026-01-21 13:06:15 -0600

    Time: 4 ms (59.77%) | Memory: 20.7 MB (7.23%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -3,6 +3,7 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        """
         i,j = 0,0
         while i<len(nums):
             while i<len(nums) and nums[i]!=0:
@@ -13,7 +14,10 @@ class Solution:
             if i<len(nums) and j<len(nums):
                 nums[i],nums[j] = nums[j], nums[i]
             i+=1
-        
-
-
+        """
+        non_zero_index = 0
+        for i in range(len(nums)):
+            if nums[i]!=0:
+                nums[non_zero_index], nums[i] = nums[i], nums[non_zero_index]
+                non_zero_index+=1
         
