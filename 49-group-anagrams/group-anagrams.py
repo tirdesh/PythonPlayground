5adb8b8e8a66cde34665c28248a3b86e39f9d53c
tirdesh <tirdesh@gmail.com>
2026-01-25 18:36:22 -0600

    Time: 11 ms (83.43%) | Memory: 24 MB (8.35%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,9 +1,13 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        c_dict = defaultdict(list)
+        mp  = {}
         for i in strs:
-            c_list = [0] * 26
+            c_list = [0]*26
             for j in i:
-                c_list[ord(j) - ord('a')] +=1
-            c_dict[tuple(c_list)].append(i)
-        return list(c_dict.values())
+                c_list[ord(j)-ord('a')] += 1
+            c_tup = tuple(c_list)
+            if c_tup not in mp:
+                mp[c_tup] = [i]
+            else:
+                mp[c_tup].append(i)
+        return [mp[i] for i in mp]
