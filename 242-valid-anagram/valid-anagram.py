b98cefc6d32f696f3c1229fa22d7f57837732c14
tirdesh <tirdesh@gmail.com>
2026-01-25 18:19:55 -0600

    Time: 11 ms (75.29%) | Memory: 19.5 MB (16.37%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,29 +1,9 @@
 from collections import defaultdict
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s)!=len(t):
-            return False
-        counts = {}
+        mp = {}
         for i in s:
-            counts[i] = counts.get(i,0) + 1
-        for j in t:
-            counts[j] = counts.get(j,0) - 1
-        return all(v==0 for v in counts.values())
-        """
-        if len(s)!=len(t):
-            return False
-        s_dict = {}
-        t_dict = {}
-        i = 0
-        while i<len(s):
-            if s[i] not in s_dict:
-                s_dict[s[i]] = 1
-            else:
-                s_dict[s[i]] += 1
-            if t[i] not in t_dict:
-                t_dict[t[i]] = 1
-            else:
-                t_dict[t[i]] += 1
-            i+=1
-        return s_dict==t_dict
-        """
+            mp[i] = mp.get(i,0)+1
+        for i in t:
+            mp[i] = mp.get(i,0)-1
+        return all(mp[i]==0 for i in mp)
