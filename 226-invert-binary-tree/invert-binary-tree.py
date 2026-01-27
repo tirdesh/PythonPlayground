d04ca8578e0747f3483aba16cf9e58fe4d8facd6
tirdesh <tirdesh@gmail.com>
2026-01-27 12:33:40 -0600

    Time: 0 ms (100.00%) | Memory: 19.4 MB (9.13%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.py
+++ 226-invert-binary-tree/invert-binary-tree.py
@@ -4,11 +4,25 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        """
         if not root:
             return None
         root.left,root.right = root.right,root.left
         self.invertTree(root.left)
         self.invertTree(root.right)
+        return root
+        """
+        if not root:
+            return None
+        queue = deque([root])
+        while queue:
+            node = queue.popleft()
+            node.left,node.right = node.right,node.left
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
         return root
