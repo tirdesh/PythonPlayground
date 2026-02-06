ba6525ef3616f16d6983fd0818c5f2bd09dd983c
tirdesh <tirdesh@gmail.com>
2026-02-06 11:40:30 -0600

    Time: 144 ms (49.57%) | Memory: 32.4 MB (14.18%) - LeetSync

--- 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.py
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.py
@@ -1,6 +1,6 @@
 class Solution:
     def minIncrementForUnique(self, nums: List[int]) -> int:
-        freq_map = [0]*(len(nums)+max(nums))
+        freq_map = [0]*(len(nums)+max(nums)+1)
         for v in nums:
             freq_map[v] += 1
         res = 0
@@ -10,8 +10,5 @@ class Solution:
                 freq_map[key] -= dup
                 freq_map[key+1] += dup
                 res+=dup
+        print([i for i,v in enumerate(freq_map) if v == 1])
         return res
-            
-
-
-            
