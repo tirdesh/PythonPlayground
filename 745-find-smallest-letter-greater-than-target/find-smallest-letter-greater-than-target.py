7cc971e49224bc78862511cfff0e35620ae05110
tirdesh <tirdesh@gmail.com>
2026-02-27 21:51:45 -0500

    Time: 0 ms (100.00%) | Memory: 20.6 MB (85.38%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.py
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.py
@@ -1,5 +1,6 @@
 class Solution:
     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        """
         freq = [0] * 26
         for i in letters:
             freq[ord(i)-ord('a')]+=1
@@ -8,3 +9,17 @@ class Solution:
             if freq[i]!=0:
                 return chr(i+ord('a'))
         return letters[0]
+        """
+        if target>=letters[-1]:
+            return letters[0]
+        lo = 0
+        hi = len(letters)-1
+        while lo<=hi:
+            mid = lo+(hi-lo)//2
+            if letters[mid]<=target:
+                lo=mid+1
+            else:
+                hi = mid-1
+        return letters[lo]
+
+
