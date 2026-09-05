e3368fe26ebe76253995560821b19e4334b038ff
tirdesh <tirdesh@gmail.com>
2026-09-05 17:37:29 -0400

    Time: 15 ms (10.14%) | Memory: 24.8 MB (20.64%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,17 +1,14 @@
-from collections import defaultdict
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        mp = defaultdict(int)
-        for i in nums:
-            mp[i]+=1
-        freq = [[] for i in range(len(nums)+1)]
-        for i,v in mp.items():
-            freq[v].append(i)
-        res = []
-        for i in range(len(freq)-1,-1,-1):
+        count = {}
+        freq = [[] for _ in range(len(nums)+1)]
+        for n in nums:
+            count[n]=1+count.get(n,0)
+        for n,c in count.items():
+            freq[c].append(n)
+        res=[]
+        for i in range(len(freq)-1,0,-1):
             for n in freq[i]:
                 res.append(n)
                 if len(res)==k:
                     return res
-
-            
